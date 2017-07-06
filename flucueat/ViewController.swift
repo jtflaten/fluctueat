584ca4346c5a702ecef80388f0a5a77b79e5b8f4
jtflaten <jtflaten@gmail.com>
2017-07-06 16:10:03 -0500

    Initial Commit

---
+++ flucueat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flucueat
+//
+//  Created by Jake Flaten on 7/6/17.
+//  Copyright © 2017 Break List. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
