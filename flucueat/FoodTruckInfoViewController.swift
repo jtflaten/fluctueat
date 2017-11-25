2a2e37f9ad5c9e0daa9da90eac174a1e68004202
jtflaten <jtflaten@gmail.com>
2017-11-25 17:15:43 -0600

    style: design
    changing the background and font of the info pages

--- flucueat/FoodTruckInfoViewController.swift
+++ flucueat/FoodTruckInfoViewController.swift
@@ -61,7 +61,7 @@ class FoodTruckInfoViewController: UIViewController, UICollectionViewDelegate, U
                 self.truckActivityIndicator.stopAnimating()
             }
         }
-        truckImage.layer.cornerRadius = 3.0
+        truckImage.layer.cornerRadius = 6.0
         truckImage.clipsToBounds = true
     }
     
@@ -107,14 +107,13 @@ class FoodTruckInfoViewController: UIViewController, UICollectionViewDelegate, U
                 return
             }
         }
-
-     
+        
         if let image = self.vendor.pictures[indexPath.row] {
-            
             cell.showImage(image: image)
-        } else {
-
-       }
+        }
+        
+        cell.layer.cornerRadius = 5.0
+        cell.clipsToBounds = true
 
         return cell
         
@@ -157,14 +156,14 @@ class FoodTruckInfoViewController: UIViewController, UICollectionViewDelegate, U
     func configureBackground() {
         
 //        let backgroundGradient = CAGradientLayer()
-      let colorTop = UIColor(red:0.90, green:0.76, blue:0.13, alpha:1.0).cgColor
-      let colorBottom = UIColor(red:0.82, green:0.01, blue:0.11, alpha: 1.0).cgColor
-//
-//        backgroundGradient.colors = [colorTop, colorBottom]
-//        backgroundGradient.locations = [0.0, 1.0]
-//        backgroundGradient.frame = view.frame
-//        view.layer.insertSublayer(backgroundGradient, at: 0)
-        view.layer.backgroundColor = colorTop
+//      let colorTop = UIColor(red:0.90, green:0.76, blue:0.13, alpha:1.0).cgColor
+//      let colorBottom = UIColor(red:0.82, green:0.01, blue:0.11, alpha: 1.0).cgColor
+////
+////        backgroundGradient.colors = [colorTop, colorBottom]
+////        backgroundGradient.locations = [0.0, 1.0]
+////        backgroundGradient.frame = view.frame
+////        view.layer.insertSublayer(backgroundGradient, at: 0)
+//        view.layer.backgroundColor = colorTop
         
     }
     
